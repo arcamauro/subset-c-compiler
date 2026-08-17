c729b7886cbe35fad58d626b02604a05a7d83211
Arcangelo Mauro <arcangelo.mauro@proton.me>
2026-08-17 13:00:32 +0200

    feature: skeleton of the main lexer (scanner) body

--- compiler/src/lexer.rs
+++ compiler/src/lexer.rs
@@ -1,5 +1,5 @@
 #[derive(Debug, PartialEq, Clone)]
-enum Token {
+enum TokenType {
     Int,
     Char,
     OpenParentheses,
@@ -7,6 +7,7 @@ enum Token {
     Semicolon,
     Return,
     If,
+    Else,
     While,
     Do,
     Switch,
@@ -14,9 +15,57 @@ enum Token {
     Comment,
     OpenBracket,
     ClosedBracket,
+    Include,
+    Define,
     Operator(String),
     Identifier(String),
     IntegerLiteral(i32),
     StringLiteral(String),
 }
 
+#[derive(Debug, PartialEq, Clone)]
+enum State {
+    Start,
+    InIdentifier,
+    InNumber,
+    InString,
+    InComment,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct Lexer {
+    input: Vec<char>,
+    position: usize,
+    state: State,
+}
+
+impl Lexer {
+    fn advance_char(&mut self) -> Option<char> {
+        if self.position < self.input.len() {
+            let character = self.input[self.position];
+            self.position += 1;
+            Some(character)
+        } else {
+            None
+        }
+    }
+    fn peek_char(&self) -> Option<char> {
+        if self.position < self.len() {
+            Some(self.input[self.position])
+        } else {
+            None
+        }
+    }
+    fn read_token(&mut self) -> Option<TokenType>{
+    
+    }
+
+    fn read_number(&mut self, character: char) -> TokenType {
+
+    }
+
+    fn read_id_or_kw(&mut self, character: char) -> TokenType {
+
+    }
+
+}
